fe5e2ba80dfd0bb316fc925e71ab6199278a7826
Anar Seferov <1anarseferov@gmail.com>
2020-12-04 19:04:51 +0400

    Updates

---
+++ precache-manifest.a9d34cf2f9b3128b2124a70917986456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce88ed63a636577cc16e146f01b61063",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "6a512aadb2016d4000d3",
+    "url": "/weather-app/static/css/main.2aa1ebd6.chunk.css"
+  },
+  {
+    "revision": "5ed5fd859d22c553a455",
+    "url": "/weather-app/static/js/2.f4723ed8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.f4723ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a512aadb2016d4000d3",
+    "url": "/weather-app/static/js/main.855a941f.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
